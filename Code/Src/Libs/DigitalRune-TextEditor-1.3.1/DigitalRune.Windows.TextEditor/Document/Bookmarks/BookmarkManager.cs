bf125c644712756a57251256c3a1ff049f1d60f5
asalvatucci <asalvatucci@gmail.com>
2022-01-07 12:58:59 +0100

    Upgraded to VS 2019, added packaging project

--- Code/Src/Libs/DigitalRune-TextEditor-1.3.1/DigitalRune.Windows.TextEditor/Document/Bookmarks/BookmarkManager.cs
+++ Code/Src/Libs/DigitalRune-TextEditor-1.3.1/DigitalRune.Windows.TextEditor/Document/Bookmarks/BookmarkManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using DigitalRune.Windows.TextEditor.Document;
-
+using DigitalRune.Windows.TextEditor.Utilities;
 
 namespace DigitalRune.Windows.TextEditor.Bookmarks
 {
--- Code/Src/Libs/DigitalRune-TextEditor-1.3.1/DigitalRune.Windows.TextEditor/Utilities/Red-Black Tree/AugmentableRedBlackTree.cs
+++ Code/Src/Libs/DigitalRune-TextEditor-1.3.1/DigitalRune.Windows.TextEditor/Utilities/Red-Black Tree/AugmentableRedBlackTree.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
-
+using System.Text;
 
 namespace DigitalRune.Windows.TextEditor.Utilities
 {
