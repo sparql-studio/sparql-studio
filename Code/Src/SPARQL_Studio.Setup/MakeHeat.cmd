3420bcb7d8e5529dd8e095c611ac7cf1181fe033
asalvatucci <asalvatucci@gmail.com>
2022-01-09 22:12:07 +0100

    prepended call to xcopy

--- Code/Src/SPARQL_Studio.Setup/MakeHeat.cmd
+++ Code/Src/SPARQL_Studio.Setup/MakeHeat.cmd
@@ -6,5 +6,6 @@ SET SourceVar=%4
 
 echo ComponentGroup = %3
 echo ComponentGroup = %ComponentGroup%
+
 echo "%WIX%\bin\heat.exe" dir "%HarvestFileDir%" -cg %ComponentGroup% -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -var var.%ComponentGroup%.SourceDir -out "%OutputPath%"
 "%WIX%\bin\heat.exe" dir %HarvestFileDir% -cg %ComponentGroup% -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -var var.%SourceVar% -out %OutputPath%
