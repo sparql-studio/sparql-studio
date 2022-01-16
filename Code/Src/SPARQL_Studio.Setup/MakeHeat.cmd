2df153e147c8ad020047ae2417f13ea35e874d06
asalvatucci <asalvatucci@gmail.com>
2022-01-16 12:36:06 +0100

    Moved all heat commands to makeheat.cmd

--- Code/Src/SPARQL_Studio.Setup/MakeHeat.cmd
+++ Code/Src/SPARQL_Studio.Setup/MakeHeat.cmd
@@ -1,8 +1,15 @@
 ﻿
-SET HarvestFileDir=%1
+SET HarvestFileDir=%1\HarvestDir
+SET HarvestSourceDir=%1\..\SPARQL_Studio\bin\%5
 SET OutputPath=%2
 SET ComponentGroup=%3
 SET SourceVar=%4
+SET Configuration=%5
+
+md "%HarvestFileDir%"
+xcopy  /E /Y "%HarvestSourceDir%\*.*" "%HarvestFIleDir%\*.*"
+del "%HarvestFIleDir%\SPARQL_Studio.exe"
+del "%HarvestFIleDir%\Sparql_document.ico"
 
 echo ComponentGroup = %3
 echo ComponentGroup = %ComponentGroup%
